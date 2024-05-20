2f6a5c5bd7776aa59e8f51ffd7a2fe7bafe96afb
張簡華芳 <167695328+ccanna@users.noreply.github.com>
2024-05-20 12:44:01 +0000

    0520

--- lesson10/lesson10_2.py
+++ lesson10/lesson10_2.py
@@ -8,6 +8,6 @@ n:int = pyip.inputInt("請輸入取得名字的數量:",min=0, max=200)
 
 names:list[str] = content.split(sep='\n')
 random_names:list[str] = random.choices(population=names, k=n)
-for name in random_names
+for name in random_names:
     print(name, end=' ')
 print()
